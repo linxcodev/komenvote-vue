b4c11cf0c60e10a80d68df30c3db177fe2e97481
Ahmad Fajrul Falah <fajrul452@gmail.com>
2018-10-20 10:22:43 +0700

    bug comment body

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 // here your code
+Vue.component('template-cs', {
+  template: '#comment-template',
+  props: ['comment'],
+  data: function() {
+    return{
+      plus: false,
+      minus: false
+    }
+  },
+  methods: {
+    sundul: function() {
+      this.plus = !this.plus
+      this.minus = false
+    },
+    gandul: function() {
+      this.minus = !this.minus
+      this.plus = false
+    }
+  },
+  computed: {
+    coto: function() {
+      if (this.plus) {
+        this.comment.coto + 1
+      } else if (this.minus) {
+        this.comment.coto - 1
+      } else {
+        this.comment.coto
+      }
+    }
+  }
+});
+
+var vm = new Vue({
+  el: '#app',
+  data: {
+    comments: [
+      { body: 'Ini komen pertama', time:'02-12-18', coto:0 },
+      { body: 'Ini komen kedua', time:'03-12-18', coto:2 },
+      { body: 'Ini komen ketiga', time:'02-14-18', coto:6 }
+    ]
+  }
+});
